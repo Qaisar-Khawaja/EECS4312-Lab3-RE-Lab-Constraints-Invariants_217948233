933b7a5b2847edcb45752fb90fbb061227ce437a
Berry <89364538+Qaisar-Khawaja@users.noreply.github.com>
2026-01-29 19:46:08 -0500

    Working on dispense.py

--- src/dispense.py
+++ src/dispense.py
@@ -17,8 +17,19 @@ class DispenseEvent:
             quantity: Number of units dispensed. Must be a positive integer.
 
         """
+        if not patient_id:
+            raise ValueError("Please provide the patient ID")
+        if dose_mg < 0:
+            raise ValueError("The dose must be a positive value.")
+        if not isinstance (quantity, int) or quantity < 0: 
+            raise Value Error("The quantity must be a psoitive integer.")
+
+        
         self.patient_id = patient_id
         self.medication = medication
+        self.dose_mg = dose_mg
+        self.quantity = quantity
+        
 
     # TODO Task 4: Define and check system invariants 
     def invariant_holds(existing_events, new_event):
